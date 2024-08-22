da2079631a4f4229d62652355681aca416cc3af8
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-08-22 16:52:08 -0300

    Add sound triggers, coin collection and animation behaviour to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,39 +6,85 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public PlayerController controller;
-
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    Animator anim;
+    public PlayerSwitch playerSwitch;
+    public LiftSpace liftSpace;
+    AudioSource source;
+    public AudioClip footstep, hop, fracture;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-        if (Input.GetButtonDown("Jump"))
+        if(playerSwitch.isPlayerActive)
         {
-            jump = true;
-        }
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if (Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
         }
-        else if (Input.GetButtonUp("Crouch"))
+        //Disable player movement is ego is active
+        else
         {
-            crouch = false;
+            horizontalMove = 0;
+            jump = false;
         }
-
     }
 
     void FixedUpdate()
     {
-        // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
+        //Only move player if is unlinked to ego
+        if (!(!playerSwitch.isPlayerActive && liftSpace.playerIsOnTop))
+        {
+            // Move player
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+            anim.SetBool("isMoving", horizontalMove != 0);
+            jump = false;
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Collect coins
+        if(collision.gameObject.tag == "Coin")
+        {
+            collision.gameObject.GetComponent<AudioSource>().Play();
+            collision.gameObject.GetComponent<Renderer>().enabled = false;
+            collision.gameObject.GetComponent<Collider2D>().enabled = false;
+            GoldManager.instance.AddGold(50);
+        }
+        
+    }
+
+
+    //Different sound states
+    public void Footstep()
+    {
+        source.clip = footstep;
+        source.Play();
+    }
+
+    public void Hop()
+    {
+        source.clip = hop;
+        source.Play();
+    }
+
+    public void Fracture()
+    {
+        source.clip = fracture;
+        source.Play();
+    }
+
 }
