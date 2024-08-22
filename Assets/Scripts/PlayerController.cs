36d5ca14906d6ad9d1f3627f87ab6ad3f4cd3f60
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-08-22 16:49:40 -0300

    Change flip player behaviour from sign saling to flipX boolean

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
     [Range(0, 1)][SerializeField] private float m_CrouchSpeed = .36f;           // Amount of maxSpeed applied to crouching movement. 1 = 100%
     [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
-    [SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
+    [SerializeField] public bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
     [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
     [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
 
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    private bool m_Grounded;            // Whether or not the player is grounded.
+    public bool m_Grounded;            // Whether or not the player is grounded.
     const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 m_Velocity = Vector3.zero;
+    Animator anim;
 
     [Header("Events")]
     [Space]
@@ -36,6 +37,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
@@ -66,6 +68,10 @@ public class PlayerController : MonoBehaviour
 
     public void Move(float move, bool crouch, bool jump)
     {
+        if(m_Rigidbody2D == null)
+        {
+            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
         // If crouching, check to see if the character can stand up
         if (!crouch)
         {
@@ -109,6 +115,10 @@ public class PlayerController : MonoBehaviour
                 }
             }
 
+            if (m_Rigidbody2D == null)
+            {
+                m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            }
             // Move the character by finding the target velocity
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
@@ -133,6 +143,14 @@ public class PlayerController : MonoBehaviour
             // Add a vertical force to the player.
             m_Grounded = false;
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            if(anim != null)
+            {
+                anim.SetBool("isGrounded", true);
+            }
+        }
+        if(anim != null)
+        {
+            anim.SetBool("isGrounded", m_Grounded);
         }
     }
 
@@ -143,8 +161,10 @@ public class PlayerController : MonoBehaviour
         m_FacingRight = !m_FacingRight;
 
         // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        //Vector3 theScale = transform.localScale;
+        //theScale.x *= -1;
+        //transform.localScale = theScale;
+
+        GetComponent<SpriteRenderer>().flipX = !m_FacingRight;
     }
 }
